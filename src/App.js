b356dac9da3327bc4d77c0d6a24563c83f0887ff
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-26 16:07:51 -0500

    Placed instructions, building out handlers.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
-import React from 'react';
+// - `<App />` will hold all the data needed for this project. It will also be the container for your Todo Components.
+//   - All of your application data will be stored here on `<App />`.
+//   - All of your `handler` functions should live here on `<App />`.
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Starting To-Do list: 
+const todo = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  },
+]
+
+// you will need a place to store your state in this component.
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super(); 
+    this.state = {
+      todo: todo,
+    }
+  }
+
+  //Toggle task completion
+  handleToggleTask = (taskID) => {
+    this.setState({
+      todo: this.state.todo.map(task => {
+        if(task.id === taskID) {
+          return {
+            ...task, 
+            completed: !task.completed,
+          }
+        } else {
+          return task;
+        };
+      })
+    });
+  };
+
+  //Add new task
+  handleAddTask = (task) => {
+    this.setState({
+      todo: [...this.state.todo, {
+        task: task,
+        id: Date.now(),
+        completed: false,
+      }]
+    });
+  };
+
+  //Clear completed tasks
+  clearTasks = () => {
+    this.setState({
+      todo: this.state.todo.filter(task => (!task.completed))
+    });
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>To-Do List!</h2>
       </div>
     );
   }
